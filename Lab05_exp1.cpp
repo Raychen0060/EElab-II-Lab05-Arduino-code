6fd9c1abc2943ea10d69cdb35d5dafc68cc5e803
RayRayChen <157879344+Raychen0060@users.noreply.github.com>
2026-03-30 23:15:53 +0800

    Create Lab05_exp1.cpp

---
+++ Lab05_exp1.cpp
@@ -0,0 +1,28 @@
+const int segA = 13; // Pin A connect 13
+const int seqD = 12; // Pin D connect 12
+int segState1 = LOW;
+int segState2 = LOW;
+unsigned long previousMillis1 = 0; 
+unsigned long previousMillis2 = 0;
+const long interval1 = 500; // because duty cycle = 50% , 0.5s changes once for 1Hz
+const long interval2 = 5; // because duty cycle = 50% , 5ms changes once for 100Hz
+
+void setup() {
+  pinMode(segA, OUTPUT);
+  pinMode(seqD, OUTPUT);
+}
+
+void loop() {
+  unsigned long currentMillis = millis();
+  if (currentMillis - previousMillis1 >= interval1) {
+    previousMillis1 = currentMillis;
+    segState1 = !segState1;
+    digitalWrite(segA, segState1);
+  }
+
+  if (currentMillis - previousMillis2 >= interval2) {
+    previousMillis2 = currentMillis;
+    segState2 = !segState2;
+    digitalWrite(segD, segState2);
+  }
+}
