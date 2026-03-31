d7b641f586be709fa420de7469f9620d0841c740
RayRayChen <157879344+Raychen0060@users.noreply.github.com>
2026-03-31 10:45:45 +0800

    Update Lab05_exp1.cpp

--- Lab05_exp1.cpp
+++ Lab05_exp1.cpp
@@ -1,5 +1,5 @@
 const int segA = 13; // Pin A connect 13
-const int segD = 12; // Pin D connect 12
+const int segD = 10; // Pin D connect 12
 int segState1 = LOW;
 int segState2 = LOW;
 unsigned long previousMillis1 = 0; 
