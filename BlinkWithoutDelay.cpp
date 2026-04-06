2d8ed848c4f2db6cb69e479bf2fe0129681ad209
RayRayChen <157879344+Raychen0060@users.noreply.github.com>
2026-04-07 00:37:14 +0800

    Update BlinkWithoutDelay.cpp

--- BlinkWithoutDelay.cpp
+++ BlinkWithoutDelay.cpp
@@ -55,17 +55,8 @@ void loop() {
   unsigned long currentMillis = millis();
 
   if (currentMillis - previousMillis >= interval) {
-    // save the last time you blinked the LED
     previousMillis = currentMillis;
-
-    // if the LED is off turn it on and vice-versa:
-    if (ledState == LOW) {
-      ledState = HIGH;
-    } else {
-      ledState = LOW;
-    }
-
-    // set the LED with the ledState of the variable:
+    ledState = ~ledState;
     digitalWrite(ledPin, ledState);
   }
 }
